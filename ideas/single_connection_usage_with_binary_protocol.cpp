8aa1a58cd674f76c638ff25898547c4e0b531a88
elsid <elsid@yandex-team.ru>
2017-10-11 12:49:57 +0300

    Add column name for binary protocol

--- ideas/single_connection_usage_with_binary_protocol.cpp
+++ ideas/single_connection_usage_with_binary_protocol.cpp
@@ -6,7 +6,7 @@ boost::asio::io_service io_service;
 
 boost::asio::spawn(io_service, [&] (boost::asio::yield_context yield) {
     auto connection_provider = apq::make_connection_provider(io_service);
-    auto query = apq::query("SELECT $1::integer + $2::integer", 13, 42);
+    auto query = apq::query("SELECT ($1::integer + $2::integer) value", 13, 42);
     auto result = apq::binary_protocol::request<Sum>(connection_provider, query, yield);
     std::cout << "sum is " << result.begin()->value << '\n';
 });
