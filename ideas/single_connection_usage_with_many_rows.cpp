406325009903d4500f3de37654ec211288d1c2f9
elsid <elsid@yandex-team.ru>
2017-10-25 23:38:49 +0300

    Example to show meaning of the fetch result

---
+++ ideas/single_connection_usage_with_many_rows.cpp
@@ -0,0 +1,12 @@
+boost::asio::io_service io_service;
+
+boost::asio::spawn(io_service, [&] (boost::asio::yield_context yield) {
+    auto connection_provider = apq::make_connection_provider(io_service);
+    apq::cursor<apq::row> cursor;
+    apq::text_protocol::request(connection_provider, cursor, "SELECT unnset(ARRAY[1, 2, 3])", yield);
+    for (apq::row row; apq::text_protocol::fetch(cursor, row, yield); ) {
+        std::cout << row.at(0) << '\n';
+    }
+});
+
+io_service.run();
