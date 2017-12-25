bd16e9254ac79d5a1399bed60874a6bf073ebb16
elsid <elsid.mail@gmail.com>
2017-12-25 15:31:48 +0300

    Fix typo

--- ideas/single_connection_usage_with_query_conf.cpp
+++ ideas/single_connection_usage_with_query_conf.cpp
@@ -21,7 +21,7 @@ boost::asio::io_service io_service;
 boost::asio::spawn(io_service, [&] (boost::asio::yield_context yield) {
     auto connection_provider = apq::make_connection_provider(io_service);
     apq::cursor cursor;
-    Summand summands;
+    Summands summands;
     summands.first = 13;
     summands.second = 42;
     auto query = apq::make_query<SumIntegers>(query_repository, summands);
