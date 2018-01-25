39793177a5ce7817d1c62a0a1b65a3e3c79c625e
elsid <elsid.mail@gmail.com>
2018-01-25 05:48:59 +0300

    Add link to coverage script to readme and add to build all script

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -2,6 +2,7 @@
 
 scripts/build_gcc_debug.sh
 scripts/build_gcc_rel_with_deb_info.sh
+scripts/build_gcc_coverage.sh
 scripts/build_clang_debug.sh
 scripts/build_clang_rel_with_deb_info.sh
 scripts/build_clang_asan.sh
