90a661562a2766b9087e00e8c312c1b7f8394ffa
Dmitry Potepalov <dpotepalov@yandex.ru>
2018-03-26 01:15:56 +0300

    Move endianness-related code to a separate file
    To be reused in binary deserialization

--- include/ozo/binary_serialization.h
+++ include/ozo/binary_serialization.h
@@ -2,6 +2,7 @@
 
 #include <ozo/concept.h>
 #include <ozo/type_traits.h>
+#include <ozo/detail/endian.h>
 
 #include <libpq-fe.h>
 
@@ -32,21 +33,6 @@ struct pg_array_dimension {
         (std::int32_t, index)
     );
 };
-
-template <class T, std::size_t ... N>
-constexpr T byte_order_swap(T value, std::index_sequence<N ...>) {
-    return (((value >> N * CHAR_BIT & std::numeric_limits<std::uint8_t>::max()) << (sizeof(T) - 1 - N) * CHAR_BIT) | ...);
-}
-
-template <class T, class U = std::make_unsigned_t<T>>
-constexpr Require<endian::native == endian::little, U> convert_to_big_endian(T value) {
-    return byte_order_swap<U>(value, std::make_index_sequence<sizeof(T)> {});
-}
-
-template <class T>
-constexpr Require<endian::native == endian::big, T> convert_to_big_endian(T value) {
-    return value;
-}
 
 template <class T, class OutIteratorT>
 constexpr Require<std::is_integral_v<T>, OutIteratorT> write(T value, OutIteratorT out) {
---
+++ include/ozo/detail/endian.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <ozo/concept.h>
+
+#include <limits.h>
+
+namespace ozo::detail {
+
+enum class endian {
+#ifdef _WIN32
+    little = 0,
+    big    = 1,
+    native = little
+#else
+    little = __ORDER_LITTLE_ENDIAN__,
+    big    = __ORDER_BIG_ENDIAN__,
+    native = __BYTE_ORDER__
+#endif
+};
+
+template <class T, std::size_t ... N>
+constexpr T byte_order_swap(T value, std::index_sequence<N ...>) {
+    return (((value >> N * CHAR_BIT & std::numeric_limits<std::uint8_t>::max()) << (sizeof(T) - 1 - N) * CHAR_BIT) | ...);
+}
+
+template <class T, class U = std::make_unsigned_t<T>>
+constexpr Require<endian::native == endian::little, U> convert_to_big_endian(T value) {
+    return byte_order_swap<U>(value, std::make_index_sequence<sizeof(T)> {});
+}
+
+template <class T>
+constexpr Require<endian::native == endian::big, T> convert_to_big_endian(T value) {
+    return value;
+}
+
+}
