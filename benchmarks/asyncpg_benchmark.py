95ecf06f881dde83383ed1d3f9a3a3a6f18c2190
Roman Siromakha <elsid@yandex-team.ru>
2018-04-30 13:52:20 +0300

    Add benchmarks

---
+++ benchmarks/asyncpg_benchmark.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import asyncio
+import asyncpg
+import time
+import sys
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
+
+
+async def run():
+    total_rows = [0]
+    start = time.monotonic()
+    await asyncio.gather(*[reuse_connection(total_rows) for _ in range(CONNECTIONS)])
+    finish = time.monotonic()
+    print('read %s rows, %.3f row/sec' % (total_rows[0], total_rows[0] / (finish - start)))
+
+
+async def reuse_connection(total_rows):
+    conn = await asyncpg.connect(sys.argv[1])
+    while total_rows[0] < 10000000:
+        values = await conn.fetch(QUERY, -1, True)
+        total_rows[0] += len(values)
+    await conn.close()
+
+
+CONNECTIONS = 8
+QUERY = '''
+SELECT typname, typnamespace, typowner, typlen, typbyval, typcategory,
+       typispreferred, typisdefined, typdelim, typrelid, typelem, typarray
+  FROM pg_type
+ WHERE typtypmod = $1 AND typisdefined = $2
+'''
+
+if __name__ == '__main__':
+    main()
---
+++ benchmarks/ozo_benchmark.cpp
@@ -0,0 +1,53 @@
+#include "benchmark.h"
+
+#include <ozo/connection_info.h>
+#include <ozo/query_builder.h>
+#include <ozo/request.h>
+
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/spawn.hpp>
+
+#include <iostream>
+#include <optional>
+
+int main(int argc, char *argv[]) {
+    using namespace ozo::literals;
+    using namespace ozo::benchmark;
+
+    namespace asio = boost::asio;
+
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <conninfo>" << std::endl;
+        return 1;
+    }
+
+    rows_count_limit_benchmark benchmark(10000000);
+    asio::io_context io(1);
+    ozo::connection_info<> connection_info(argv[1]);
+    const auto query = ("SELECT typname, typnamespace, typowner, typlen, typbyval, typcategory, "_SQL +
+                        "typispreferred, typisdefined, typdelim, typrelid, typelem, typarray "_SQL +
+                        "FROM pg_type WHERE typtypmod = "_SQL +
+                        -1 + " AND typisdefined = "_SQL + true).build();
+
+    for (int i = 0; i < 8; ++i) {
+        asio::spawn(io, [&] (auto yield) {
+            try {
+                auto connection = ozo::get_connection(ozo::make_connector(connection_info, io), yield);
+                benchmark.start();
+                while (true) {
+                    ozo::result result;
+                    ozo::request(connection, query, std::ref(result), yield);
+                    if (!benchmark.step(result.size())) {
+                        break;
+                    }
+                }
+            } catch (const std::exception& e) {
+                std::cout << e.what() << '\n';
+            }
+        });
+    }
+
+    io.run();
+
+    return 0;
+}
