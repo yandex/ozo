844b88a9b0074a536dbb6581641258db229138ed
Roman Siromakha <elsid@yandex-team.ru>
2018-05-08 15:02:57 -0600

    Request function with async result

---
+++ include/ozo/request.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <ozo/impl/async_request.h>
+
+namespace ozo {
+
+template <typename P, typename Q, typename Out, typename CompletionToken, typename = Require<ConnectionProvider<P>>>
+inline auto request(P&& provider, Q&& query, Out&& out, CompletionToken&& token) {
+    using signature_t = void (error_code, connectable_type<P>);
+    async_completion<CompletionToken, signature_t> init(token);
+
+    async_request(std::forward<P>(provider), std::forward<Q>(query), std::forward<Out>(out),
+                  init.completion_handler);
+
+    return init.result.get();
+}
+
+} // namespace ozo
