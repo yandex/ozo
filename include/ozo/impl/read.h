8d0d5a39408dd8f2767d3929c8e6bd5c1825378c
Sergei L. Khandrikov <thed636@yandex-team.ru>
2018-06-01 14:13:23 +0300

    Add typed_buffer helper union to avoid reinterpret_cast
    typed_buffer allows to avoid reinterpret cast of temporary while reading
    it.

--- include/ozo/impl/read.h
+++ include/ozo/impl/read.h
@@ -2,19 +2,28 @@
 
 #include <ozo/concept.h>
 #include <ozo/detail/istream.h>
+#include <ozo/detail/typed_buffer.h>
 
 namespace ozo::impl {
 
 using detail::istream;
 
 template <typename T>
-inline Require<Integral<T>, istream&> read(istream& in, T& out) {
-    T tmp;
-    in.read(reinterpret_cast<char*>(std::addressof(tmp)), sizeof out);
+inline Require<RawDataWritable<T>, istream&> read(istream& in, T& out) {
+    using std::data;
+    using std::size;
+    in.read(data(out), size(out));
     if (!in) {
         throw system_error(error::unexpected_eof);
     }
-    out = detail::convert_from_big_endian(tmp);
+    return in;
+}
+
+template <typename T>
+inline Require<Integral<T>, istream&> read(istream& in, T& out) {
+    detail::typed_buffer<T> buf;
+    read(in, buf);
+    out = detail::convert_from_big_endian(buf.typed);
     return in;
 }
 
@@ -23,15 +32,6 @@ inline Require<FloatingPoint<T>, istream&> read(istream& in, T& out) {
     detail::floating_point_integral_t<T> tmp;
     read(in, tmp);
     out = detail::to_floating_point(tmp);
-    return in;
-}
-
-template <typename T>
-inline Require<RawDataWritable<T>, istream&> read(istream& in, T& out) {
-    in.read(out.data(), out.size());
-    if (!in) {
-        throw system_error(error::unexpected_eof);
-    }
     return in;
 }
 
