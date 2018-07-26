74e0122d523386295cd4fe95e1e28b7507b20672
Roman Siromakha <elsid@yandex-team.ru>
2018-07-26 20:28:00 +0300

    Connection provider does not require to contain io context

--- include/ozo/connection_info.h
+++ include/ozo/connection_info.h
@@ -10,30 +10,48 @@ namespace ozo {
 template <
     typename OidMap = empty_oid_map,
     typename Statistics = no_statistics>
-class connection_info {
-    io_context& io_;
-    std::string conn_str_;
-    Statistics statistics_;
-
+struct connection_info {
     using connection = impl::connection_impl<OidMap, Statistics>;
-
-public:
     using connection_type = std::shared_ptr<connection>;
 
-    connection_info(io_context& io, std::string conn_str,
-            Statistics statistics = Statistics{})
-    : io_(io), conn_str_(std::move(conn_str)), statistics_(std::move(statistics)) {}
+    std::string conn_str;
+    Statistics statistics;
 
-    template <typename Handler>
-    void async_get_connection(Handler&& h) const {
-        impl::async_connect(conn_str_,
-            std::make_shared<connection>(io_, statistics_),
-            std::forward<Handler>(h));
-    }
+    connection_info(std::string conn_str, Statistics statistics = Statistics{})
+            : conn_str(std::move(conn_str)), statistics(std::move(statistics)) {}
 };
 
-static_assert(
-    ConnectionProvider<connection_info<>>,
-    "connection_info does not fit ConnectionProvider concept");
+static_assert(ConnectionSource<connection_info<>>, "is not a ConnectionSource");
+
+template <typename OidMap = empty_oid_map, typename Statistics = no_statistics>
+class connection_info_connector {
+public:
+    using source_type = connection_info<OidMap, Statistics>;
+    using connection = typename source_type::connection;
+    using connection_type = typename source_type::connection_type;
+
+    connection_info_connector(const source_type& base, io_context& io)
+            : base(base), io(io) {}
+
+    template <typename Handler>
+    void async_get_connection(Handler&& handler) const {
+        impl::async_connect(
+            base.conn_str,
+            std::make_shared<connection>(io, base.statistics),
+            std::forward<Handler>(handler)
+        );
+    }
+
+private:
+    const source_type& base;
+    io_context& io;
+};
+
+static_assert(ConnectionProvider<connection_info_connector<>>, "is not a ConnectionProvider");
+
+template <typename OidMap, typename Statistics>
+auto make_connector(const connection_info<OidMap, Statistics>& base, io_context& io) {
+    return connection_info_connector<OidMap, Statistics>(base, io);
+}
 
 } // namespace ozo
