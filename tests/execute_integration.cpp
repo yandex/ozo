8bd770d3b931e54b2a145a40decbb692b4b7a841
Roman Siromakha <elsid@yandex-team.ru>
2018-08-16 21:48:10 +0300

    Add execute operation

---
+++ tests/execute_integration.cpp
@@ -0,0 +1,26 @@
+#include <ozo/connection_info.h>
+#include <ozo/query_builder.h>
+#include <ozo/execute.h>
+
+#include <gtest/gtest.h>
+
+namespace {
+
+using namespace testing;
+
+TEST(execute, should_perform_operation_without_result) {
+    using namespace ozo::literals;
+
+    ozo::io_context io;
+    ozo::connection_info<> conn_info(OZO_PG_TEST_CONNINFO);
+
+    ozo::execute(ozo::make_connector(conn_info, io), "BEGIN"_SQL,
+        [&](ozo::error_code ec, auto conn) {
+            ASSERT_FALSE(ec) << ec.message() << " | " << error_message(conn) << " | " << get_error_context(conn);
+            EXPECT_FALSE(ozo::connection_bad(conn));
+        });
+
+    io.run();
+}
+
+} // namespace
