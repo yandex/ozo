d625f40fddecc9bda0fd3ce929a6a928bcc6002c
Sergei L. Khandrikov <thed636@yandex-team.ru>
2018-08-21 10:01:07 +0300

    Basic Doxygen/Doxypress style in source documentation

--- include/ozo/concept.h
+++ include/ozo/concept.h
@@ -14,8 +14,31 @@
 namespace ozo {
 
 /**
-* This is requirement simulation type, which is the alias to std::enable_if_t
-* It is pretty simple to use it with pseudo-concepts such as OperatorNot below.
+ * @defgroup Concepts
+ * @brief Library-wide concepts emulation mechanism
+ */
+///@{
+/**
+* @brief Concept requirement emulation
+*
+* This is requirement simulation type, which is the alias to std::enable_if_t.
+* It is pretty simple to use it with pseudo-concepts such as OperatorNot, Iterable and so on.
+* E.g. overloading functions via Require:
+* @code
+    template <typename T>
+    decltype(auto) unwrap(T&& c, Require<!Nullable<T>>* = 0) {
+        return c;
+    }
+
+    template <typename T>
+    decltype(auto) unwrap(T&& c, Require<Nullable<T>>* = 0) {
+        return *c;
+    }
+* @endcode
+*
+* @tparam Condition - logical combination of concepts.
+* @tparam Type - return type, *void* by default.
+* @returns Type if Condition is true
 */
 template <bool Condition, typename Type = void>
 using Require = std::enable_if_t<Condition, Type>;
@@ -27,13 +50,15 @@ template <typename T>
 struct has_operator_not<T, std::void_t<decltype(!std::declval<T>())>>
     : std::true_type {};
 
+/**
+ * @brief Operator Not concept
+ *
+ * Return true if T has operator !(), false in other case
+ * @tparam T - type to examine
+ */
 template <typename T>
 constexpr auto OperatorNot = has_operator_not<std::decay_t<T>>::value;
 
-/**
-* This trait determines whether T is tagged
-* as a output iterator
-*/
 template <typename T, typename Enable = void>
 struct is_output_iterator : std::false_type {};
 
@@ -46,13 +71,15 @@ struct is_output_iterator<T, typename std::enable_if<
 >::type>
 : std::true_type {};
 
+/**
+ * @brief Output Iterator concept
+ *
+ * Returns true if T is output iterator
+ * @tparam T - type to examine
+ */
 template <typename T>
 constexpr auto OutputIterator = is_output_iterator<T>::value;
 
-/**
-* This trait determines whether T is tagged
-* as a forward iterator
-*/
 template <typename T, typename Enable = void>
 struct is_forward_iterator : std::false_type {};
 
@@ -65,30 +92,34 @@ struct is_forward_iterator<T, typename std::enable_if<
 >::type>
 : std::true_type {};
 
+/**
+ * @brief Forward Iterator concept
+ *
+ * Returns true if T meets forward_iterator conditions
+ * @tparam T - type to examine
+ */
 template <typename T>
 constexpr auto ForwardIterator = is_forward_iterator<T>::value;
 
-/**
- * This trait determines whether T can be iterated through
- * via begin() end() functions
- */
 template <typename T, typename Enable = void>
 struct is_iterable : std::false_type {};
 
 template <typename T>
 struct is_iterable<T, typename std::enable_if<
-    is_forward_iterator<decltype(std::declval<T>().begin())>::value &&
-        is_forward_iterator<decltype(std::declval<T>().end())>::value
+    is_forward_iterator<decltype(begin(std::declval<T>()))>::value &&
+        is_forward_iterator<decltype(end(std::declval<T>()))>::value
 >::type>
 : std::true_type {};
 
+/**
+ * @brief Iterable concept
+ *
+ * Determines whether T can be iterated through via begin() end() functions
+ * @tparam T - type to examine
+ */
 template <typename T>
 constexpr auto Iterable = is_iterable<T>::value;
 
-/**
-* This trait determines whether T is
-* an insert iterator bound with some container
-*/
 template <typename T, typename Enable = void>
 struct is_insert_iterator : std::false_type {};
 
@@ -98,21 +129,57 @@ struct is_insert_iterator<T, typename std::enable_if<
 >::type>
 : std::true_type {};
 
+/**
+ * @brief Insert Iterator concept
+ *
+ * This trait determines whether T is an insert iterator bound with some container
+ * @tparam T - type to examine
+ */
 template <typename T>
 constexpr auto InsertIterator = is_insert_iterator<T>::value;
 
+/**
+ * @brief Boost.Fusion Sequence concept
+ *
+ * Indicates if T is a Boost.Fusion Sequence type
+ * @tparam T - type to examine
+ */
 template <typename T>
 constexpr auto FusionSequence = boost::fusion::traits::is_sequence<std::decay_t<T>>::value;
 
+/**
+ * @brief Boost.Hana Sequence concept
+ *
+ * Indicates if T is a Boost.Hana Sequence type
+ * @tparam T - type to examine
+ */
 template <typename T>
 constexpr auto HanaSequence = boost::hana::Sequence<std::decay_t<T>>::value;
 
+/**
+ * @brief Boost.Hana Structure concept
+ *
+ * Indicates if T is a Boost.Hana Structure type
+ * @tparam T - type to examine
+ */
 template <typename T>
 constexpr auto HanaStruct = boost::hana::Struct<std::decay_t<T>>::value;
 
+/**
+ * @brief Boost.Hana String concept
+ *
+ * Indicates if T is a Boost.Hana String type
+ * @tparam T - type to examine
+ */
 template <typename T>
 constexpr auto HanaString = decltype(boost::hana::is_a<boost::hana::string_tag>(std::declval<T>()))::value;
 
+/**
+ * @brief Boost.Hana Tuple concept
+ *
+ * Indicates if T is a Boost.Hana Tuple type
+ * @tparam T - type to examine
+ */
 template <typename T>
 constexpr auto HanaTuple = decltype(boost::hana::is_a<boost::hana::tuple_tag>(std::declval<T>()))::value;
 
@@ -124,12 +191,31 @@ struct is_fusion_adapted_struct<T,
     std::void_t<decltype(boost::fusion::extension::struct_member_name<T, 0>::call())>
 > : std::true_type {};
 
+
+/**
+ * @brief Boost.Fusion Adapted Structure concept
+ *
+ * Indicates it T is a Boost.Fusion adapted structure via BOOST_FUSION_ADAPT_STRUCT or similar.
+ * @tparam T - type to examine
+ */
 template <typename T>
 constexpr auto FusionAdaptedStruct = is_fusion_adapted_struct<std::decay_t<T>>::value;
 
+/**
+ * @brief Integral concept
+ *
+ * Indicates if T is an integral type
+ * @tparam T - type to examine
+ */
 template <typename T>
 constexpr auto Integral = std::is_integral_v<std::decay_t<T>>;
 
+/**
+ * @brief Floating Point concept
+ *
+ * Indicates if T is a floating point type
+ * @tparam T - type to examine
+ */
 template <typename T>
 constexpr auto FloatingPoint = std::is_floating_point_v<std::decay_t<T>>;
 
@@ -139,6 +225,12 @@ struct is_raw_data_writable : std::false_type {};
 template <typename T>
 struct is_raw_data_writable<T, std::void_t<decltype(std::declval<T&>().data())>> : std::true_type {};
 
+/**
+ * @brief Raw Data Writable concept
+ *
+ * Indicates if T can be written as a sequence of bytes without endian conversion
+ * @tparam T - type to examine
+ */
 template <typename T>
 constexpr auto RawDataWritable = is_raw_data_writable<std::decay_t<T>>::value;
 
@@ -148,7 +240,15 @@ struct is_emplaceable : std::false_type {};
 template <typename T>
 struct is_emplaceable<T, std::void_t<decltype(std::declval<T&>().emplace())>> : std::true_type {};
 
+/**
+ * @brief Emplaceable concept
+ *
+ * Inticates if container T can emplace it's element with default constructor
+ * @tparam T - type to examine
+ */
 template <typename T>
 constexpr auto Emplaceable = is_emplaceable<std::decay_t<T>>::value;
+
+///@}
 
 } // namespace ozo
