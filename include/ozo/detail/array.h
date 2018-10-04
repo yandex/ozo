fb062cccdaebd39b115dbbdae6e39a71311a66a5
Sergei L. Khandrikov <thed636@yandex-team.ru>
2018-10-04 18:33:06 +0300

    Add array binary representation size calculation

--- include/ozo/detail/array.h
+++ include/ozo/detail/array.h
@@ -2,7 +2,11 @@
 
 #include <ozo/type_traits.h>
 #include <boost/hana/adapt_struct.hpp>
+#include <boost/hana/members.hpp>
+#include <boost/hana/size.hpp>
+#include <boost/hana/fold.hpp>
 #include <boost/fusion/adapted/struct/adapt_struct.hpp>
+#include <boost/range/numeric.hpp>
 
 namespace ozo::detail {
 
@@ -20,6 +24,33 @@ struct pg_array_dimension {
         (std::int32_t, index)
     );
 };
+
+template <typename T>
+struct size_of_array_impl {
+    constexpr static size_type data_size(const T& v) {
+        using ozo::size_of;
+        if constexpr (StaticSize<typename T::value_type>) {
+            return std::empty(v) ? 0 : (sizeof(size_type) + size_of(*std::begin(v))) * std::size(v);
+        }
+        return boost::accumulate(v, size_type(0),
+            [&] (auto r, const auto& item) { return r + sizeof(size_type) + size_of(item);});
+    }
+
+    static constexpr auto apply(const T& v) {
+        constexpr const auto header_size = hana::fold(
+            hana::members(pg_array{}), size_type(0),
+            [&] (auto r, const auto& item) { return r + sizeof(item); });
+
+        constexpr const auto dimension_header_size = hana::fold(
+            hana::members(pg_array_dimension{}), size_type(0),
+            [&] (auto r, const auto& item) { return r + sizeof(item); });
+
+        return header_size + dimension_header_size + data_size(v);
+    }
+};
+
+template <typename T>
+struct size_of_impl_dispatcher<T, Require<Array<T>>> { using type = size_of_array_impl<std::decay_t<T>>; };
 
 } // namespace ozo::detail
 
