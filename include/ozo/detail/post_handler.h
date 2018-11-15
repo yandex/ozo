d3cddbf0fe24e94aec142be6231f5b99b29dbe1e
Sergei L. Khandrikov <thed636@yandex-team.ru>
2018-11-15 16:55:45 +0300

    Remove make_*** functions in favor of constructors
    C++17 allows to resolve class template parameters via constructors,
    let's use it instead of dummy make_*** boilerplate.

--- include/ozo/detail/post_handler.h
+++ include/ozo/detail/post_handler.h
@@ -12,17 +12,13 @@ template <typename Handler>
 struct post_handler {
     Handler handler;
 
+    post_handler(Handler handler) : handler(std::move(handler)) {}
+
     template <typename Connection>
     void operator() (error_code ec, Connection&& connection) {
         decltype(auto) io = get_io_context(connection);
         asio::post(io, detail::bind(std::move(handler), std::move(ec), std::forward<Connection>(connection)));
     }
 };
-
-template <typename Handler>
-inline auto make_post_handler(Handler&& handler) {
-    using result_type = post_handler<std::decay_t<Handler>>;
-    return result_type {std::forward<Handler>(handler)};
-}
 
 } // namespace ozo::detail
