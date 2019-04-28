6197c7b2077df7d143d69e8361bf83a4c5e9f95f
Sergei L. Khandrikov <orionstation@yandex.ru>
2019-04-29 01:59:14 +0300

    Add now() function for the time traits.
    It will be needed for deadline calculations.

--- include/ozo/time_traits.h
+++ include/ozo/time_traits.h
@@ -11,6 +11,14 @@ namespace ozo {
 struct time_traits {
     using duration = std::chrono::steady_clock::duration; //!< Time duration type of the library
     using time_point = std::chrono::steady_clock::time_point; //!< Time point type of the library
+    /**
+     * Get current time
+     *
+     * @return time_point --- current time
+     */
+    static time_point now() noexcept(noexcept(std::chrono::steady_clock::now())) {
+        return std::chrono::steady_clock::now();
+    }
 };
 
-} // namespace apq
+} // namespace ozo
