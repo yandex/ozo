58059aff79662831951e704eca761ae1696621a6
Sergei L. Khandrikov <orionstation@yandex.ru>
2019-04-29 01:59:14 +0300

    Add now() for time traits and no_time_constrain
    no_time_constrain_t - indicates that no timer is needed to control
    operation time.

--- include/ozo/time_traits.h
+++ include/ozo/time_traits.h
@@ -11,6 +11,18 @@ namespace ozo {
 struct time_traits {
     using duration = std::chrono::steady_clock::duration; //!< Time duration type of the library
     using time_point = std::chrono::steady_clock::time_point; //!< Time point type of the library
+    /**
+     * Get current time
+     *
+     * @return time_point --- current time
+     */
+    static time_point now() noexcept(noexcept(std::chrono::steady_clock::now())) {
+        return std::chrono::steady_clock::now();
+    }
 };
 
-} // namespace apq
+struct no_time_constrain_t {};
+
+constexpr auto no_time_constrain = no_time_constrain_t{};
+
+} // namespace ozo
