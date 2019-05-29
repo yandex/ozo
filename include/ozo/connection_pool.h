868ba5c9720c962fee12954005d8f0a046f09b74
Sergei L. Khandrikov <thed636@yandex-team.ru>
2019-05-29 18:59:32 +0300

    Gcc treats using derpecated type in deprecated function as a warning :(

--- include/ozo/connection_pool.h
+++ include/ozo/connection_pool.h
@@ -28,7 +28,7 @@ struct connection_pool_config {
  * Time restrictions to get connection from the `ozo::connection_pool`
  * @note This type is deprecated, use time constrained version of ozo::get_connection() or ozo::dedline for operations.
  */
-struct [[deprecated]] connection_pool_timeouts {
+struct /*[[deprecated]]*/ connection_pool_timeouts {
     time_traits::duration connect = std::chrono::seconds(10); //!< maximum time interval to establish to or wait for free connection with DBMS
     time_traits::duration queue = std::chrono::seconds(10); //!< [[IGNORED]]
 };
