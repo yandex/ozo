2b3d3914575024b0b9eecf5fd89ae4f0bbd57339
Sergei L. Khandrikov <thed636@yandex-team.ru>
2019-10-01 19:04:02 +0300

    Rename cancel_socket->cancel_io to conform underlying entity changes

--- tests/detail/timeout_handler.cpp
+++ tests/detail/timeout_handler.cpp
@@ -12,11 +12,11 @@ struct connection_mock {
     MOCK_METHOD0(cancel, void());
 };
 
-TEST(cancel_socket, should_cancel_socket_io) {
+TEST(cancel_io, should_cancel_socket_io) {
     connection_mock conn;
-    ozo::detail::cancel_socket cancel_socket_handler{conn, std::allocator<char>{}};
+    ozo::detail::cancel_io cancel_io_handler{conn, std::allocator<char>{}};
     EXPECT_CALL(conn, cancel()).WillOnce(Return());
-    cancel_socket_handler(ozo::error_code{});
+    cancel_io_handler(ozo::error_code{});
 }
 
 } // namespace
