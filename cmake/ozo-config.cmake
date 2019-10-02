d90d1fd5db211d40bfcbf8bf58c78f62196193ad
JonasS <jonass@dev.jsje.de>
2019-10-02 18:12:53 +0200

    Fix usage for external projects - find_package(ozo) will now resolve dependencies on it's own - fixed include path when ozo is externally resolved - moved installed tareget to yandex:: namespace - vendored FindPostgreSQL of CMake 3.14 - added regression test to ensure ozo can be used from external projects

--- cmake/ozo-config.cmake
+++ cmake/ozo-config.cmake
@@ -1,1 +1,13 @@
+include(CMakeFindDependencyMacro)
+
+# Find PostgreSQL using new provided script
+# Remove this once we move to CMake 3.14+
+set(CMAKE_MODULE_PATH_old_ ${CMAKE_MODULE_PATH})
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_LIST_DIR}")
+find_dependency(PostgreSQL)
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH_old_})
+
+find_dependency(Boost COMPONENTS coroutine context system thread atomic)
+find_dependency(resource_pool)
+
 include("${CMAKE_CURRENT_LIST_DIR}/ozo-targets.cmake")
