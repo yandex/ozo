8dc86aa42e7aca627a9abdbcd25008687f846950
Roman Siromakha <elsid.mail@gmail.com>
2019-10-15 21:53:05 +0200

    Use a function to run example docker-compose

--- scripts/run_example.sh
+++ scripts/run_example.sh
@@ -4,19 +4,17 @@ scripts/build.sh pg docker clang release
 
 docker-compose up -d ozo_postgres
 
-docker-compose run \
-    --rm \
-    --user "$(id -u):$(id -g)" \
-    ozo_build_with_pg_tests \
-    bash \
-    -exc '/code/scripts/wait_postgres.sh; ${BASE_BUILD_DIR}/clang_release/examples/ozo_request_coroutine "host=${POSTGRES_HOST} user=${POSTGRES_USER} dbname=${POSTGRES_DB} password=${POSTGRES_PASSWORD}"'
+function run_example {
+    docker-compose run \
+        --rm \
+        --user "$(id -u):$(id -g)" \
+        ozo_build_with_pg_tests \
+        bash \
+        -exc "/code/scripts/wait_postgres.sh; \${BASE_BUILD_DIR}/clang_release/examples/${1:?} \"host=\${POSTGRES_HOST:?} user=\${POSTGRES_USER:?} dbname=\${POSTGRES_DB:?} password=\${POSTGRES_PASSWORD:?}\""
+}
 
-docker-compose run \
-    --rm \
-    --user "$(id -u):$(id -g)" \
-    ozo_build_with_pg_tests \
-    bash \
-    -exc '/code/scripts/wait_postgres.sh; ${BASE_BUILD_DIR}/clang_release/examples/ozo_connection_pool "host=${POSTGRES_HOST} user=${POSTGRES_USER} dbname=${POSTGRES_DB} password=${POSTGRES_PASSWORD}"'
+run_example ozo_request_coroutine
+run_example ozo_connection_pool
 
 docker-compose stop ozo_postgres
 docker-compose rm -f ozo_postgres
