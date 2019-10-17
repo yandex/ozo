2c319ec2d47a00296667cc929c32b4f9b60b17a3
Roman Siromakha <elsid.mail@gmail.com>
2019-10-17 21:08:06 +0200

    Use a funciton to run benchmark docker-compose

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -4,41 +4,21 @@ docker-compose build asyncpg aiopg ozo_build_with_pg_tests
 
 scripts/build.sh pg docker clang release
 
-echo 'asyncpg benchmark'
+function run_benchmark {
+    docker-compose run \
+        --rm \
+        --user "$(id -u):$(id -g)" \
+        ${1:?} \
+        bash \
+        -exc "/code/scripts/wait_postgres.sh; ${2:?} ${3:?}"
+}
 
-docker-compose run \
-    --rm \
-    --user "$(id -u):$(id -g)" \
-    asyncpg \
-    bash \
-    -exc '/code/scripts/wait_postgres.sh; benchmarks/asyncpg_benchmark.py "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DB}"'
-
-echo 'aiopg benchmark'
-
-docker-compose run \
-    --rm \
-    --user "$(id -u):$(id -g)" \
-    aiopg \
-    bash \
-    -exc '/code/scripts/wait_postgres.sh; benchmarks/aiopg_benchmark.py "host=${POSTGRES_HOST} user=${POSTGRES_USER} dbname=${POSTGRES_DB} password=${POSTGRES_PASSWORD}"'
-
-echo 'ozo benchmark'
-
-docker-compose run \
-    --rm \
-    --user "$(id -u):$(id -g)" \
-    ozo_build_with_pg_tests \
-    bash \
-    -exc '/code/scripts/wait_postgres.sh; ${BASE_BUILD_DIR}/clang_release/benchmarks/ozo_benchmark "host=${POSTGRES_HOST} user=${POSTGRES_USER} dbname=${POSTGRES_DB} password=${POSTGRES_PASSWORD}"'
-
-echo 'ozo performance benchmark'
-
-docker-compose run \
-    --rm \
-    --user "$(id -u):$(id -g)" \
-    ozo_build_with_pg_tests \
-    bash \
-    -exc '/code/scripts/wait_postgres.sh; ${BASE_BUILD_DIR}/clang_release/benchmarks/ozo_benchmark_performance "host=${POSTGRES_HOST} user=${POSTGRES_USER} dbname=${POSTGRES_DB} password=${POSTGRES_PASSWORD}"'
+run_benchmark asyncpg benchmarks/asyncpg_benchmark.py '"postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DB}"'
+run_benchmark aiopg benchmarks/aiopg_benchmark.py '"host=${POSTGRES_HOST} user=${POSTGRES_USER} dbname=${POSTGRES_DB} password=${POSTGRES_PASSWORD}"'
+run_benchmark ozo_build_with_pg_tests '${BASE_BUILD_DIR}/clang_release/benchmarks/ozo_benchmark' \
+    '"host=${POSTGRES_HOST} user=${POSTGRES_USER} dbname=${POSTGRES_DB} password=${POSTGRES_PASSWORD}"'
+run_benchmark ozo_build_with_pg_tests '${BASE_BUILD_DIR}/clang_release/benchmarks/ozo_benchmark_performance' \
+    '"host=${POSTGRES_HOST} user=${POSTGRES_USER} dbname=${POSTGRES_DB} password=${POSTGRES_PASSWORD}"'
 
 docker-compose stop ozo_postgres
 docker-compose rm -f ozo_postgres
