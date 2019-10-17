98a9b3035c8e948c4dac6efe0a3388db1e36a906
Roman Siromakha <elsid.mail@gmail.com>
2019-10-17 22:33:28 +0200

    Allow to run performance benchmark with custom parameters

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -17,8 +17,21 @@ run_benchmark asyncpg benchmarks/asyncpg_benchmark.py '"postgresql://${POSTGRES_
 run_benchmark aiopg benchmarks/aiopg_benchmark.py '"host=${POSTGRES_HOST} user=${POSTGRES_USER} dbname=${POSTGRES_DB} password=${POSTGRES_PASSWORD}"'
 run_benchmark ozo_build_with_pg_tests '${BASE_BUILD_DIR}/clang_release/benchmarks/ozo_benchmark' \
     '"host=${POSTGRES_HOST} user=${POSTGRES_USER} dbname=${POSTGRES_DB} password=${POSTGRES_PASSWORD}"'
-run_benchmark ozo_build_with_pg_tests '${BASE_BUILD_DIR}/clang_release/benchmarks/ozo_benchmark_performance' \
-    '"host=${POSTGRES_HOST} user=${POSTGRES_USER} dbname=${POSTGRES_DB} password=${POSTGRES_PASSWORD}"'
+
+function run_ozo_benchmark_performance {
+    run_benchmark ozo_build_with_pg_tests '${BASE_BUILD_DIR}/clang_release/benchmarks/ozo_benchmark_performance' \
+        "${1:?} --conninfo=\"host=\${POSTGRES_HOST} user=\${POSTGRES_USER} dbname=\${POSTGRES_DB} password=\${POSTGRES_PASSWORD}\""
+}
+
+run_ozo_benchmark_performance '--benchmark=reopen_connection --query=simple'
+run_ozo_benchmark_performance '--benchmark=reuse_connection --query=simple'
+run_ozo_benchmark_performance '--benchmark=use_connection_pool --query=simple --coroutines=1'
+run_ozo_benchmark_performance '--benchmark=use_connection_pool --query=simple --coroutines=2'
+run_ozo_benchmark_performance '--benchmark=use_connection_pool_mult_threads --query=simple --coroutines=2 --threads=2 --connections=4 --queue=0'
+run_ozo_benchmark_performance '--benchmark=use_connection_pool_mult_threads --query=simple --coroutines=2 --threads=2 --connections=2 --queue=4'
+run_ozo_benchmark_performance '--benchmark=use_connection_pool --query=simple --coroutines=1 --parse'
+run_ozo_benchmark_performance '--benchmark=use_connection_pool --query=complex --coroutines=1'
+run_ozo_benchmark_performance '--benchmark=use_connection_pool --query=complex --coroutines=1 --parse'
 
 docker-compose stop ozo_postgres
 docker-compose rm -f ozo_postgres
