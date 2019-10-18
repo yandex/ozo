86eba120b997f4fbadac00672c0ce5b692dfa0bd
Sergei L. Khandrikov <thed636@yandex-team.ru>
2019-10-18 12:33:09 +0300

    Reduce memory allocations for OIDs request while connect

--- include/ozo/impl/request_oid_map.h
+++ include/ozo/impl/request_oid_map.h
@@ -10,12 +10,11 @@ inline T unwrap_type_c(hana::basic_type<T>);
 
 template <typename Impl>
 inline auto get_types_names(const oid_map_t<Impl>& oid_map) {
-    std::vector<std::string> retval;
-    retval.reserve(hana::length(oid_map.impl));
-    hana::for_each(hana::keys(oid_map.impl), [&] (const auto& key) {
-        retval.push_back(type_name<decltype(unwrap_type_c(key))>());
+    return hana::unpack(hana::keys(oid_map.impl), [](auto const& ...x) {
+        return std::array<std::string_view, sizeof...(x)>{
+            {type_name<decltype(unwrap_type_c(x))>()...}
+        };
     });
-    return retval;
 }
 
 template <typename Impl>
@@ -42,7 +41,9 @@ inline void set_oid_map(oid_map_t<Impl>& oid_map, const oids_result& res) {
         if (*i == null_oid) {
             throw std::invalid_argument(
                 std::string("null oid for type ")
-                + boost::core::demangle(typeid(key).name()));
+                + boost::core::demangle(typeid(key).name())
+                + " which is mapped as "
+                + type_name<decltype(unwrap_type_c(key))>());
         }
         oid_map.impl[key] = *i;
         ++i;
@@ -51,41 +52,49 @@ inline void set_oid_map(oid_map_t<Impl>& oid_map, const oids_result& res) {
 
 template <typename Handler>
 struct request_oid_map_op {
-    Handler handler_;
-    std::shared_ptr<oids_result> res_;
+    struct context {
+        Handler handler_;
+        oids_result res_;
+        context(Handler&& handler)
+        : handler_(std::move(handler)) {}
+    };
 
-    request_oid_map_op(Handler handler)
-    : handler_(std::move(handler)),
-      res_(std::allocate_shared<oids_result>(get_allocator())) {}
+    std::shared_ptr<context> ctx_;
+
+    request_oid_map_op(Handler handler) {
+        auto allocator = asio::get_associated_allocator(handler);
+        ctx_ = std::allocate_shared<context>(allocator, std::move(handler));
+    }
 
     template <typename Connection>
     void perform(Connection&& conn) {
         const auto oid_map = ozo::unwrap_connection(conn).oid_map();
+        ctx_->res_.reserve(hana::length(oid_map.impl));
         async_request(std::forward<Connection>(conn), make_oids_query(oid_map),
-            none, std::back_inserter(*res_), std::move(*this));
+            none, std::back_inserter(ctx_->res_), std::move(*this));
     }
 
     template <typename Connection>
     void operator() (error_code ec, Connection&& conn) {
         if (!ec) try {
-            set_oid_map(ozo::unwrap_connection(conn).oid_map(), *res_);
+            set_oid_map(ozo::unwrap_connection(conn).oid_map(), ctx_->res_);
         } catch (const std::exception& e) {
             unwrap_connection(conn).set_error_context(e.what());
             ec = error::oid_request_failed;
         }
-        handler_(ec, std::forward<Connection>(conn));
+        ctx_->handler_(ec, std::forward<Connection>(conn));
     }
 
-    using executor_type = decltype(asio::get_associated_executor(handler_));
+    using executor_type = asio::associated_executor_t<Handler>;
 
     executor_type get_executor() const noexcept {
-        return asio::get_associated_executor(handler_);
+        return asio::get_associated_executor(ctx_->handler_);
     }
 
-    using allocator_type = decltype(asio::get_associated_allocator(handler_));
+    using allocator_type = asio::associated_allocator_t<Handler>;
 
     allocator_type get_allocator() const noexcept {
-        return asio::get_associated_allocator(handler_);
+        return asio::get_associated_allocator(ctx_->handler_);
     }
 };
 
