edd06ccbd7abbbb75e694f4f4082741ac73961bc
Sergei L. Khandrikov <orionstation@yandex.ru>
2020-03-29 00:06:11 +0300

    Substitute std::istream with custom one for faster deserialization
    std::istream is heavy constructible, so ligthwight alternative should solve
    this problem.

--- include/ozo/detail/istream.h
+++ include/ozo/detail/istream.h
@@ -4,27 +4,51 @@
 
 namespace ozo::detail {
 
-struct istream : std::istream {
-    using std::istream::istream;
-};
-
-class istreambuf_view : public std::streambuf {
+class istreambuf {
+    const char* i_;
+    const char* last_;
 public:
-    istreambuf_view(const char* data, size_t len) {
-        // Standard streambuf contains only non constant char pointers for
-        // buffer. So we need to do here a const_cast. However, we do not
-        // allow put back characters since we do not override pbackfail().
-        auto buf = const_cast<char*>(data);
-        setg(buf, buf, buf + len);
-    }
 
-protected:
-    std::streamsize xsgetn(char* s, std::streamsize n) override {
-        n = std::min(n, egptr() - gptr());
-        std::copy(gptr(), gptr() + n, s);
-        gbump(n);
+    constexpr istreambuf(const char* data, size_t len) noexcept
+    : i_(data), last_(data + len) {}
+
+    std::streamsize read(char* buf, std::streamsize n) noexcept {
+        auto last = std::min(i_ + n, last_);
+        std::copy(i_, last, buf);
+        n = std::distance(i_, last);
+        i_ = last;
         return n;
     }
+};
+
+class istream {
+public:
+    using traits_type = std::istream::traits_type;
+    using char_type = std::istream::char_type;
+
+    constexpr istream(const char* data, size_t len) noexcept
+    : buf_(data, len) {}
+
+    istream& read(char_type* buf, std::streamsize len) noexcept {
+        std::streamsize nbytes = buf_.read(buf, len);
+        if (nbytes != len) {
+            unexpected_eof_ = true;
+        }
+        return *this;
+    }
+
+    traits_type::int_type get() noexcept {
+        char retval;
+        if (!read(&retval, 1)) {
+            return traits_type::eof();
+        }
+        return retval;
+    }
+
+    operator bool() const noexcept { return !unexpected_eof_;}
+private:
+    istreambuf buf_;
+    bool unexpected_eof_ = false;
 };
 
 } // namespace ozo::detail
